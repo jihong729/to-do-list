eb0df2723332490d97eb8cf5784cd20963f15516
Jihong Min <jihong729@naver.com>
2022-05-08 10:22:19 +0900

    Use find() method to render DB items

--- app.js
+++ app.js
@@ -32,19 +32,20 @@ const item3 = new Item({
 
 const defaultItems = [item1, item2, item3];
 
-Item.insertMany(defaultItems, function(err){
-  if (err){
-    console.log(err);
-  } else {
-    console.log("Successfully saved default items to DB.");
-  }
-});
+// Item.insertMany(defaultItems, function(err){
+//   if (err){
+//     console.log(err);
+//   } else {
+//     console.log("Successfully saved default items to DB.");
+//   }
+// });
+
 
 app.get("/", function(req, res) {
 
-  res.render("list", {
-    listTitle: "Today",
-    newListItems: items
+  Item.find({}, function(err, foundItems){
+    res.render("list", {  listTitle: "Today",  newListItems: foundItems
+    });
   });
 
 });
